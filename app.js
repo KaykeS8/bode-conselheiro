1cccde322069215f82f2dbcb1ecaa5350dbd623c
kayke <kayke.py@gmail.com>
2022-02-11 14:46:31 -0300

    Arquivo com o codigo javascript para a funcionalidade do projeto

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
+const msg = document.querySelector(".msg")
 
+console.log(msg)
 
 async function fraseBode() {
-    const url = 'https://api.adviceslip.com/advice'
-    const response = await fetch(url)
-    const fraseJson = await response.json()
-
-    console.log(fraseJson)
-    console.log(fraseJson.slip.advice)
+    try  {
+        const url = 'https://api.adviceslip.com/advice'
+        const response = await fetch(url)
+        const fraseJson = await response.json()
+    
+        console.log(fraseJson)
+        console.log(fraseJson.slip.advice)
+    } catch(erro) {
+        console.log(erro)
+    }
 }
 
 fraseBode()
